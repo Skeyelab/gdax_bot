d08d2aa7113eb9109c55d48fdd5d3642d443b789
Eric Dahl <eric@skeyelab.com>
2018-02-21 10:33:54 -0600

    helpers

--- inc/helpers.rb
+++ inc/helpers.rb
@@ -27,6 +27,15 @@ module GetKey
 end
 
 
+def humanize secs
+  [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+    if secs > 0
+      secs, n = secs.divmod(count)
+      "#{n.to_i} #{name}"
+    end
+  }.compact.reverse.join(' ')
+end
+
 class Numeric
   def percent_of(n)
     self.to_f / n.to_f * 100.0
