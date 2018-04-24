fdbf7c2f6533ae57f7a52a754166654ceddfc651
Eric Dahl <eric@skeyelab.com>
2018-04-24 10:59:31 -0500

    Rubocop: Layout/Tab

--- inc/close_position.rb
+++ inc/close_position.rb
@@ -1,45 +1,45 @@
 def close_position (order, price)
-	 rest_api = Coinbase::Exchange::Client.new(ENV['GDAX_TOKEN'], ENV['GDAX_SECRET'], ENV['GDAX_PW'], product_id: order.product_id )
+  rest_api = Coinbase::Exchange::Client.new(ENV['GDAX_TOKEN'], ENV['GDAX_SECRET'], ENV['GDAX_PW'], product_id: order.product_id )
 
-	 if !watch_order(order)
- 		 return false
- 	end
-	 order_size = order.size
+  if !watch_order(order)
+    return false
+  end
+  order_size = order.size
 
-	 if order["side"] == "buy"
- 		 puts ""
- 		 puts "Selling #{order_size.to_f} #{order['product_id'].split('-')[0]} @ $#{price}"
+  if order["side"] == "buy"
+    puts ""
+    puts "Selling #{order_size.to_f} #{order['product_id'].split('-')[0]} @ $#{price}"
 
- 		 begin
-  			 sell_order = rest_api.sell(order_size,price)
-   			#puts "Sell order ID is #{sell_order.id}"
-   			#puts sell_order
-   			watch_order sell_order
-   			return
+    begin
+      sell_order = rest_api.sell(order_size,price)
+      #puts "Sell order ID is #{sell_order.id}"
+      #puts sell_order
+      watch_order sell_order
+      return
 
     rescue Coinbase::Exchange::NotFoundError => e
-   			if e.message == "{\"message\":\"NotFound\"}"
-   				 puts "Order not found"
-   				 sleep 1
-   				 return
-   			end
+      if e.message == "{\"message\":\"NotFound\"}"
+        puts "Order not found"
+        sleep 1
+        return
+      end
 
     rescue Exception => e
-  			 #binding.pry
-  		end
+      #binding.pry
+    end
 
- 		 # elsif order["side"] == "sell"
- 		 #   puts "Selling #{order_size} #{order_size} #{order['product_id'].split('-')[0]} @ $#{price}"
- 		 #   begin
- 		 #     sell_order = rest_api.buy(order_size,price) do |resp|
- 		 #       puts "Sell order ID is #{resp.id}"
+    # elsif order["side"] == "sell"
+    #   puts "Selling #{order_size} #{order_size} #{order['product_id'].split('-')[0]} @ $#{price}"
+    #   begin
+    #     sell_order = rest_api.buy(order_size,price) do |resp|
+    #       puts "Sell order ID is #{resp.id}"
 
- 		 #       return sell_order
- 		 #     end
- 		 #   rescue Exception => e
- 		 #     binding.pry
- 		 #   end
- 		 print "\a"
+    #       return sell_order
+    #     end
+    #   rescue Exception => e
+    #     binding.pry
+    #   end
+    print "\a"
 
- 	end
+  end
 end
