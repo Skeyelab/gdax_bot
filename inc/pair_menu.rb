1b346456164ff230249b2c71c9783295dd23fa09
Eric Dahl <eric@skeyelab.com>
2018-08-29 20:10:29 -0500

    oops

--- inc/pair_menu.rb
+++ inc/pair_menu.rb
@@ -3,5 +3,5 @@
 def pair_menu
   prompt = TTY::Prompt.new
   choices = %w[LTC-BTC ETH-BTC BCH-BTC BTC-USD ETH-USD LTC-USD BCH-USD ETC-BTC Back]
-  prompt.enum_select('Pair?', choices, per_page: 8)
+  prompt.enum_select('Pair?', choices, per_page: 9)
 end
