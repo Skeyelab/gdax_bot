d691ba3ace6c2dcd14b030415feef6782282e04c
Eric Dahl <eric@skeyelab.com>
2018-11-28 12:40:26 -0600

    standard cleanup

--- inc/market_skim.rb
+++ inc/market_skim.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-def market_skim(times = 2, pair = 'ETH-BTC', percent_of_portfolio = 0.99, down = 0.00002, up = 0.00002)
+def market_skim(times = 2, pair = "ETH-BTC", percent_of_portfolio = 0.99, down = 0.00002, up = 0.00002)
   redis = Redis.new
   # rest_api = Coinbase::Exchange::Client.new(ENV['GDAX_TOKEN'], ENV['GDAX_SECRET'], ENV['GDAX_PW'], product_id: pair)
   # while true
   times.times do
-    spot = format('%.5f', redis.get("spot_#{pair.split('-')[0]}_#{pair.split('-')[1]}"))
+    spot = format("%.5f", redis.get("spot_#{pair.split("-")[0]}_#{pair.split("-")[1]}"))
     puts "Spot: #{spot}"
     begin
       open_position((spot.to_f - down).round_down(5), (spot.to_f + up).round_down(5), percent_of_portfolio, pair)
-    rescue StandardError => exception
+    rescue => exception
       puts exception
       break
     end
