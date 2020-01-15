c8c537b14de1d30d01a7caf943a66e3ba89a1673
Eric Dahl <eric@skeyelab.com>
2020-01-15 12:10:40 -0600

    Update schedule.rb

--- inc/schedule.rb
+++ inc/schedule.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 scheduler = Rufus::Scheduler.new
+redis = Redis.new
 
 scheduler.every '10m' do
-  takeProfitTo redis.get('ProfitTo').to_f
+    #puts "Taking profit to #{redis.get('ProfitTo')}"
+    takeProfitTo redis.get('ProfitTo').to_f
 end
