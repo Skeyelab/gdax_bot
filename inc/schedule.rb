ec964334c379afa50d3b23b25493bc2116d893e2
Eric Dahl <eric@skeyelab.com>
2020-01-15 12:15:12 -0600

    Update schedule.rb

--- inc/schedule.rb
+++ inc/schedule.rb
@@ -3,8 +3,7 @@
 scheduler = Rufus::Scheduler.new
 redis = Redis.new
 
-
 scheduler.every '10m' do
-    #puts "Taking profit to #{redis.get('ProfitTo')}"
-    #takeProfitTo redis.get('ProfitTo').to_f
+  # puts "Taking profit to #{redis.get('ProfitTo')}"
+  # takeProfitTo redis.get('ProfitTo').to_f
 end
