eedfe9f32cb1c1e30c05f6a2550bba23f7f98b68
Eric Dahl <eric@skeyelab.com>
2020-02-06 19:36:48 -0600

    Update schedule.rb

--- inc/schedule.rb
+++ inc/schedule.rb
@@ -4,9 +4,11 @@ begin
   scheduler = Rufus::Scheduler.new(lockfile: '.rufus-scheduler.lock')
   redis = Redis.new
 
-  scheduler.every '10m' do
-    takeProfitTo redis.get('ProfitTo').to_f if redis.get('takeProfits') == 'true'
+  scheduler.cron '*/3 * * * *' do
+    if redis.get('takeProfits') == 'true'
+      takeProfitTo redis.get('ProfitTo').to_f
+    end
   end
 rescue Rufus::Scheduler::NotRunningError => e
-  puts "scheduler already running"
+  puts 'scheduler already running'
 end
