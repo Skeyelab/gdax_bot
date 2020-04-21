62897a024d254bcf6d1575a578b03f0a3115981c
Eric Dahl <eric@skeyelab.com>
2020-04-21 12:16:18 -0500

    Update buy.rb

--- inc/buy.rb
+++ inc/buy.rb
@@ -20,7 +20,6 @@ def buy(pair, price, order_size)
       buy_order
     end
   rescue Coinbase::Pro::BadRequestError => e
-    
   rescue StandardError => e
     Raven.capture_exception(e)
     # puts e
