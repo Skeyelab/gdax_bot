6cfc5e406e21dab2b4845657009ddacee0811a37
Eric Dahl <eric@skeyelab.com>
2020-04-22 10:01:58 -0500

    Update cb.rb
    Fixes SKEYELAB-A

--- classes/cb.rb
+++ classes/cb.rb
@@ -4,18 +4,33 @@
 class Cb
   def self.balance
     rest_api = Coinbase::Pro::Client.new(ENV['GDAX_TOKEN'], ENV['GDAX_SECRET'], ENV['GDAX_PW'])
-    rest_api.coinbase_accounts.each do |cba|
-      return cba['balance'].to_f if cba['name'] == 'USD Wallet'
+    begin
+      rest_api.coinbase_accounts.each do |cba|
+        return cba['balance'].to_f if cba['name'] == 'USD Wallet'
+      end
+    rescue Coinbase::Pro::RateLimitError
+      sleep 1
+      retry
     end
   end
 
   def self.withdraw(dollars)
     rest_api = Coinbase::Pro::Client.new(ENV['GDAX_TOKEN'], ENV['GDAX_SECRET'], ENV['GDAX_PW'])
-    rest_api.coinbase_withdrawal(dollars, 'USD', ENV['CB_WALLET_ID'])
+    begin
+      rest_api.coinbase_withdrawal(dollars, 'USD', ENV['CB_WALLET_ID'])
+    rescue Coinbase::Pro::RateLimitError
+      sleep 1
+      retry
+    end
   end
 
   def self.deposit(dollars)
     rest_api = Coinbase::Pro::Client.new(ENV['GDAX_TOKEN'], ENV['GDAX_SECRET'], ENV['GDAX_PW'])
-    rest_api.coinbase_deposit(dollars, 'USD', ENV['CB_WALLET_ID'])
+    begin
+      rest_api.coinbase_deposit(dollars, 'USD', ENV['CB_WALLET_ID'])
+    rescue Coinbase::Pro::RateLimitError
+      sleep 1
+      retry
+    end
   end
 end
