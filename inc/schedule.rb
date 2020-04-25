0e4cdddc457d2d7d97a9cd2a462005983e21bd20
Eric Dahl <eric@skeyelab.com>
2020-04-25 14:37:06 -0500

    Update schedule.rb

--- inc/schedule.rb
+++ inc/schedule.rb
@@ -4,16 +4,18 @@ begin
   scheduler = Rufus::Scheduler.new(lockfile: '.rufus-scheduler.lock')
   redis = Redis.new
 
+  scheduler.every '30m' do
+    $loader.reload
+  end
+
   scheduler.cron '*/3 * * * *' do
     if redis.get('takeProfits') == 'true'
-
       begin
         takeProfitTo redis.get('ProfitTo').to_f
       rescue StandardError => e
         Raven.capture_exception(e)
         raise e
       end
-
     end
   end
 rescue Rufus::Scheduler::NotRunningError => e
