07131acc7cf1422b049cbd43c25bbac14c193fce
Eric Dahl <eric@skeyelab.com>
2020-04-25 20:09:39 -0500

    Update helpers.rb

--- inc/helpers.rb
+++ inc/helpers.rb
@@ -179,6 +179,7 @@ def totalBalanceInUsd
     rescue Net::OpenTimeout => e
       sleep 1
       retry
+    rescue TypeError
     rescue Exception => e
       Raven.capture_exception(e)
       # puts e
