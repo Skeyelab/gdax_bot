be727993a88e03623f535aab5e57e2d086279761
Eric Dahl <eric@goenvoy.co>
2021-02-22 17:48:11 -0500

    Update schedule.rb

--- inc/schedule.rb
+++ inc/schedule.rb
@@ -13,8 +13,8 @@ begin
     if redis.get('takeProfits') == 'true'
       begin
         takeProfitTo redis.get('ProfitTo').to_f
-        if totalBalanceInUsd < (redis.get('ProfitTo').to_f * redis.get('stopPercent').to_f)
-#binding.pry
+        if (totalBalanceInUsd < (redis.get('ProfitTo').to_f * redis.get('stopPercent').to_f)) && orderz.count.zero?
+          # binding.pry
           redis.set('spread', 0.000)
           redis.set('BTC_split', 0)
           redis.set('LTC_split', 0)
